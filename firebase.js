7876504accd6a3d8f7202ae63a07bdab0a82130d
dockyard-le-bistro <kamrasavi2@gmail.com>
2026-07-19 02:36:30 -0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAuaFGwNjJtV0mcXZ7Tw734R0Q4HzChs1k",
+  authDomain: "dockyard-le-bistro.firebaseapp.com",
+  databaseURL: "https://dockyard-le-bistro-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "dockyard-le-bistro",
+  storageBucket: "dockyard-le-bistro.firebasestorage.app",
+  messagingSenderId: "991617661975",
+  appId: "1:991617661975:web:9d69822a65b19fe31a3052"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
